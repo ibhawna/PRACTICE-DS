817977678ef77d055e943f13dbca6c702b7eef03
ichbhawna <rathibhawna18@gmail.com>
2021-08-17 10:53:18 +0530

    initial commit

---
+++ given a range (L-R) print XOR.cpp
@@ -0,0 +1,11 @@
+// L =2
+// R =4
+// XOR??
+
+ans : XOR(R) ^ XOR(L-1);
+
+// XOR(N) = 
+// if(n % 4 == 0)  print n
+// if(n % 4 == 1)  print 1
+// if(n % 4 == 2)  print n+1
+// if(n % 4 == 3)  print 0
