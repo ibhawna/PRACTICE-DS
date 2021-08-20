b029ba56b3f9599e2a2389b65f33effbd688b7ef
ichbhawna <rathibhawna18@gmail.com>
2021-08-20 23:09:53 +0530

    initial commit

--- 22. Detect if two integers have opposite signs.cpp
+++ 22. Detect if two integers have opposite signs.cpp
@@ -1,4 +1,12 @@
+
+// 1.
 bool oppositeSigns(int x, int y)
 {
     return ((x ^ y) < 0);
 }
+
+// 2.
+bool oppositeSigns(int x, int y){
+    return (x^y)>> 31 ;
+}
+The function is written only for compilers where size of an integer is 32 bit.
