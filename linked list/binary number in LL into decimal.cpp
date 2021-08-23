9283dae95556365272acd3fa6ed14d35be46d60b
ichbhawna <rathibhawna18@gmail.com>
2021-08-23 12:33:48 +0530

    initial commit

--- linked list/binary number in LL into decimal.cpp
+++ linked list/binary number in LL into decimal.cpp
@@ -6,3 +6,38 @@
         }
         return ans;
     }
+
+    // recursive
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int getDecimalValue(ListNode* head) {
+        // int ans = 0;
+        // while(head){
+        //     // ans = (ans<<1);
+        //     // ans = ans | head->val;
+        //     ans = ans*2 + head->val;
+        //     head =head->next;
+        // }
+        // return ans;
+        if(head == NULL){
+            return ans;
+        }
+        else{
+            ans =  (ans*2 + head->val);
+            getDecimalValue(head->next);
+        }
+       return ans;
+        
+    }
+};
