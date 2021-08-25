ce1bfb44c019665a46e329f94fdf8864f4d85e38
ichbhawna <rathibhawna18@gmail.com>
2021-08-25 12:43:34 +0530

    initial commit

---
+++ leetcode dsa bootcamp kunal kushwaha/1929. Concatenation of Array.cpp
@@ -0,0 +1,31 @@
+// o(n) and o(1) approach
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        //o(1)
+        int n = nums.size();
+        for(int i =0; i<n; i++){
+            nums.push_back(nums[i]);
+        }
+        return nums;
+        
+    }
+};
+
+//o(n) and o(n) approach
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(2*(nums.size()), 0);
+        
+        for(int i =0; i<nums.size(); i++){
+            ans[i]= nums[i];
+        }
+        for(int i =0; i<nums.size(); i++){
+            ans[i+nums.size()]= nums[i];
+        }
+        
+        return ans;
+    }
+};
